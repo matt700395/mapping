d8208a6bd8c98fb2a17f099b70fcac7c4c6e7f27
matt700395 <86474039+matt700395@users.noreply.github.com>
2022-10-17 20:16:45 +0900

    edit center point3

--- map.js
+++ map.js
@@ -1,8 +1,26 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(36.60103530000024, 127.29169749999987), // 지도의 중심좌표
+        center: new kakao.maps.LatLng(36.601659208879646, 127.29777601594054), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+
+//마커코드----------------------------------------------------------------
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 마커가 표시될 위치입니다 
+var markerPosition  = new kakao.maps.LatLng(36.601659208879646, 127.29777601594054); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+// 아래 코드는 지도 위의 마커를 제거하는 코드입니다
+// marker.setMap(null);    
