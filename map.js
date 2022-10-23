0a3752464d30d937cc2146f0669124ce4288e80d
matt700395 <86474039+matt700395@users.noreply.github.com>
2022-10-23 16:53:39 +0900

    인포윈도우 삭제

--- map.js
+++ map.js
@@ -170,14 +170,14 @@ function addMarker(position, normalOrigin, overOrigin, clickOrigin, title) {
 
     // 마커에 표시할 인포윈도우를 생성합니다 
     var infowindow = new kakao.maps.InfoWindow({
-        content: title // 인포윈도우에 표시할 내용
+        //content: title // 인포윈도우에 표시할 내용
     });
 
     // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
     // 이벤트 리스너로는 클로저를 만들어 등록합니다 
     // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-    kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
-    kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+    // kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+    // kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
 
 }
 
