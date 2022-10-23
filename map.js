55bee75ad4c5ad8ea8d7a2c4a9d232dce0ec2edc
isdiscodead <isdiscodead@korea.ac.kr>
2022-10-23 11:04:36 +0900

    css

--- map.js
+++ map.js
@@ -14,9 +14,15 @@ var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니
 // 마커가 표시될 위치입니다 
 var markerPosition  = new kakao.maps.LatLng(36.601659208879646, 127.29777601594054); 
 
+var imageSrc = 'pin2.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(30, 30), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+
 // 마커를 생성합니다
 var marker = new kakao.maps.Marker({
-    position: markerPosition
+    position: markerPosition,
+    image: markerImage
 });
 
 // 마커가 지도 위에 표시되도록 설정합니다
