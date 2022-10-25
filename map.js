ccb904a218c76e2c990717e9224727f26940770f
isdiscodead <isdiscodead@korea.ac.kr>
2022-10-26 01:27:56 +0900

    edited map and css

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(36.601659208879646, 127.29777601594054), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        level: 6 // 지도의 확대 레벨
     };
 
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
