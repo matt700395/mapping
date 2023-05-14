abb264aa4a58286523834747bef80d83ce68d604
yewon <ye7648@gmail.com>
2023-05-14 22:54:19 +0900

    server.js

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ server.use(express.urlencoded({ extended: true }));
 
 server.use('/user', userRouter);
 
+
 server.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url}  라우터가 없습니다.`);
     error.status = 404;
